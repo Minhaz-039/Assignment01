2e8afe7588e4a9c2d1e61bb2f06ce8d941ae0007
Minhaz-039 <minhazreza3243@gmail.com>
2025-05-08 23:50:13 +0600

    Logo conflict

--- src/Helpers/LogoShow.jsx
+++ src/Helpers/LogoShow.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import Logo from "../assets/Logo/logo.png";
 
 export default function LogoShow() {
   return (
     <a href="#" className="flex items-center">
-      <img src="/src/assets/Logo/logo.svg" className="h-6 mr-3 sm:h-9" />
+      <img src={Logo} className="h-6 mr-3 sm:h-9" />
       <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
         Learn with Sumit
       </span>
